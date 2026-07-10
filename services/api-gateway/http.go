6c2a7c0745a9b7b9a7843282d469dc963491ebed
ShakthiW <shakthi.20221274@iit.ac.lk>
2026-07-10 12:51:42 +0530

    feat: add TripService gRPC definition and api-gateway client integration

--- services/api-gateway/http.go
+++ services/api-gateway/http.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	grpcclients "ride-sharing/services/api-gateway/grpc_clients"
 	"ride-sharing/shared/contracts"
 )
 
@@ -25,6 +26,14 @@ func handleTripPreview(w http.ResponseWriter, r *http.Request) {
 
 	jsonBody, _ := json.Marshal(reqBody)
 	reader := bytes.NewReader(jsonBody)
+
+	tripService, err := grpcclients.NewTripServiceClient()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer tripService.Close()
+
+	// tripService.Client.PreviewTrip()
 
 	// TODO: Call Trip Service
 	url := "http://trip-service:8083/preview"
