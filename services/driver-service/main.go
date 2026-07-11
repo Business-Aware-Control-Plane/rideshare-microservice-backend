0646c537d247063447b5051c921b093d30a978d1
ShakthiW <shakthi.20221274@iit.ac.lk>
2026-07-11 11:42:39 +0530

    feat: implement driver-service gRPC server with basic registration interface and build configuration

--- services/driver-service/main.go
+++ services/driver-service/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "log"
+import (
+	"context"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	grpcserver "google.golang.org/grpc"
+)
+
+var GrpcAddr = ":9092"
 
 func main() {
-	log.Println("Hello World")
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go func() {
+		sigCh := make(chan os.Signal, 1)
+		signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
+		<-sigCh
+		cancel()
+	}()
+
+	lis, err := net.Listen("tcp", GrpcAddr)
+	if err != nil {
+		log.Fatalf("Failed to Listen: %v", err)
+	}
+
+	service := NewService()
+
+	grpcServer := grpcserver.NewServer()
+	NewGrpcHandler(grpcServer, service)
+
+	log.Printf("Starting gRPC server Driver Service on Port: %s ", lis.Addr().String())
+
+	go func() {
+		if err := grpcServer.Serve(lis); err != nil {
+			log.Printf("Failed to serve gRPC server: %v", err)
+			cancel()
+		}
+	}()
+
+	// Wait for the shutdown Signal
+	<-ctx.Done()
+	log.Println("Shutting down the server...")
+	grpcServer.GracefulStop()
 }
