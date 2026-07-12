f7ec5775d6d98a7dbd7206bc8bd2a88cf32605ae
ShakthiW <shakthi.20221274@iit.ac.lk>
2026-07-12 20:03:44 +0530

    feat: implement RabbitMQ consumer in API gateway to forward async events to WebSockets

--- services/api-gateway/ws.go
+++ services/api-gateway/ws.go
@@ -13,7 +13,7 @@ var (
 	connManager = messaging.NewConnectionManager()
 )
 
-func HandleRidersWebSocket(w http.ResponseWriter, r *http.Request) {
+func handleRidersWebSocket(w http.ResponseWriter, r *http.Request, rb *messaging.RabbitMQ) {
 	conn, err := connManager.Upgrade(w, r)
 	if err != nil {
 		log.Printf("WebSocket upgrade failed: %v", err)
@@ -43,7 +43,7 @@ func HandleRidersWebSocket(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func HandleDriversWebSocket(w http.ResponseWriter, r *http.Request) {
+func handleDriversWebSocket(w http.ResponseWriter, r *http.Request, rb *messaging.RabbitMQ) {
 	conn, err := connManager.Upgrade(w, r)
 	if err != nil {
 		log.Printf("WebSocket upgrade failed: %v", err)
@@ -103,6 +103,19 @@ func HandleDriversWebSocket(w http.ResponseWriter, r *http.Request) {
 	}); err != nil {
 		log.Printf("Error Sending Message: %v", err)
 		return
+	}
+
+	// Initialize queue consumers
+	queues := []string{
+		messaging.DriverCmdTripRequestQueue,
+	}
+
+	for _, q := range queues {
+		consumer := messaging.NewQueueConsumer(rb, connManager, q)
+
+		if err := consumer.Start(); err != nil {
+			log.Printf("Failed to start consumer for queue: %s: err: %v", q, err)
+		}
 	}
 
 	for {
